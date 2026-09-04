1f8e3a6059bc656086eafe3d84886b8cb8578c80
Karol John <delacruzkaroljohn012@gmail.com>
2026-09-04 12:59:57 +0800

    Initialize Spring backend

---
+++ backend/src/main/java/group/four/nyare/nyare/NyareApplication.java
@@ -0,0 +1,13 @@
+package group.four.nyare.nyare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/group/four/nyare/nyare/NyareApplicationTests.java
@@ -0,0 +1,13 @@
+package group.four.nyare.nyare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
